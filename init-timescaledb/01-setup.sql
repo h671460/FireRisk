2e4bb7cca219f010643329e162eaf97992e72a66
Yosafe Fesaha Oqbamecail <581515@stud.hvl.no>
2026-04-12 22:03:14 +0200

    frcm-ape kub-ted

--- init-timescaledb/01-setup.sql
+++ init-timescaledb/01-setup.sql
@@ -1,7 +1,4 @@
--- Enable TimescaleDB extension
 CREATE EXTENSION IF NOT EXISTS timescaledb;
-
--- fire_risk data table
 CREATE TABLE IF NOT EXISTS fire_risk (
   time          TIMESTAMPTZ      NOT NULL,
   lat           DOUBLE PRECISION NOT NULL,
@@ -15,11 +12,8 @@ CREATE TABLE IF NOT EXISTS fire_risk (
   created_at    TIMESTAMPTZ      NOT NULL DEFAULT now(),
   PRIMARY KEY (time, lat, lon, created_at)
 );
-
 SELECT create_hypertable('fire_risk', 'time', if_not_exists => TRUE);
-
 CREATE INDEX IF NOT EXISTS fire_risk_location_time_idx
   ON fire_risk (location, time DESC);
-
 CREATE INDEX IF NOT EXISTS fire_risk_time_idx
   ON fire_risk (time DESC);
