86ec86fd60d417bde2b18c097d3319e736bf522a
JonasStokkeland <js.jonas97@gmail.com>
2026-04-19 20:15:27 +0200

    Add test methods for the has_roles method in auth.py

--- tests/test_src/test_firerisk/test_api/test_auth.py
+++ tests/test_src/test_firerisk/test_api/test_auth.py
@@ -1,4 +1,41 @@
 import pytest
+from fastapi import HTTPException
+from firerisk.api.routers.auth import has_roles
+from src.firerisk.api.schemas.userPayload import userPayload
 
-def test_has_roles():
+@pytest.mark.asyncio
+async def test_has_roles():
+
+    user = userPayload(
+        id ="1",
+        username = "test",
+        email ="test@test.com",
+        first_name = "Test",
+        last_name = "User",
+        realm_roles = ["admin"]    
+    )
+
+    checker = has_roles(["admin"])
     
+    result = await checker(user)
+
+    assert result == user
+
+@pytest.mark.asyncio
+async def test_has_roles_forbidden():
+
+    user = userPayload(
+        id ="1",
+        username = "test",
+        email ="test@test.com",
+        first_name = "Test",
+        last_name = "User",
+        realm_roles = ["developer"]    
+    )
+
+    checker = has_roles(["admin"])
+    
+    with pytest.raises(HTTPException) as exc:
+        await checker(user)
+
+    assert exc.value.status_code == 403
