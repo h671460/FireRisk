d5003c67ac1e961c24d1a3881544d62368122453
Yosafe Fesaha Oqbamecail <581515@stud.hvl.no>
2026-05-07 15:51:09 +0200

    updated api

--- src/firerisk/api/main.py
+++ src/firerisk/api/main.py
@@ -43,7 +43,7 @@ def health_check():
 
 @app.get("/healthy2")
 def another_health_check():
-    return {'status': 'Healthy88'}
+    return {'status': 'Healthy66'}
 
 app.include_router(frcm_route.router)
 app.include_router(user.router)
