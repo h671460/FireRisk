c8bcc116780b95ec26993fa3fd48bb82ba40cccf
Yosafe Fesaha Oqbamecail <581515@stud.hvl.no>
2026-05-08 07:35:00 +0200

    some fe changes

--- frontend_keycloak/src/keycloak-config.js
+++ frontend_keycloak/src/keycloak-config.js
@@ -10,7 +10,11 @@ const initOptions = {
     clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID || "frcm-react-app-client",
 };
 
-
+// const initOptions = {
+//     url: "https://fireriskgroup02.com/auth/",
+//     realm: "frcm-realm",
+//     clientId: "frcm-react-app-client",
+// };
 
 console.log("Keycloak Init Options:", initOptions); // Debugging line to check the config values
 
